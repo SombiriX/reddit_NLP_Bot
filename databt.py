89fee8551612c09e024d597d9f088afbb84ce260
Sombiri Enwemeka <sombiri@enwemeka.com>
2017-06-04 12:23:05 -0700

    - Added / updated function comments     - Ran Spellcheck - Renamed update_reddit_comments to get_reddit_comments - Moved comment file check to get_reddit_comments function - Renamed comment_data to subm_data for clarity - Changed submission data to list of dict objects rather than dict of dicts     - Simplifies deserializing the resulting JSON object in R - Shortened time output for debug logging

--- databt.py
+++ databt.py
@@ -19,12 +19,22 @@ sys.path.append(os.path.join(os.getcwd(), '..'))
 
 
 def norm(text):
+    '''
+    Cleans up encoding for input text
+    Input: Text to normalize
+    Return: Normalized text
+    '''
     if text is None:
         return None
     return normalize('NFKD', text).encode('ascii', 'ignore')
 
 
 def test_redditor(submission):
+    '''
+    Make sure the intended value exists
+    Input: PRAW submission object
+    Return: Normalized submission author name
+    '''
     if submission is None:
         return None
     if submission.author is None:
@@ -34,13 +44,12 @@ def test_redditor(submission):
 
 def get_entity_sentiment(text):
     '''
-    Detects entity sentiment in the provided text.
+    Detects entities and sentiment about them from the provided text.
+    Input: Body of text to analyze
+    Return: List of entities sorted on salience (relevance to text body)
     '''
     language_client = language_service_client.LanguageServiceClient()
     document = language_service_pb2.Document()
-
-    #if isinstance(text, six.binary_type):
-    #    text = text.decode('utf-8')
 
     document.content = text.encode('utf-8')
     document.type = enums.Document.Type.PLAIN_TEXT
@@ -63,7 +72,26 @@ def get_entity_sentiment(text):
     return sorted(e_list, key=itemgetter('salience'), reverse=True)
 
 
-def update_reddit_comments(args):
+def get_reddit_comments(args):
+    '''
+    Uses PRAW to access reddit and download comments and associated data
+    checks if comment file already exist or the requested number of submissions
+    has been updated to prevent unnecessary queries.
+    Input: Argparse args this program was called with
+    Return: Comment data dictionary
+    Note: This function writes the downloaded comments to a JSON file
+    '''
+    # Check whether to update comment data
+    subm_data = []
+    if os.path.isfile(args.o):
+        f = open(args.o, "r")
+        subm_data = json.loads(f.read())
+        if not subm_data[-1] is args.n:
+            del(subm_data)
+            subm_data = get_reddit_comments(args)
+        f.close()
+        return subm_data
+
     api_keys = None
     try:
         with open(args.k, 'r') as fp:
@@ -79,11 +107,22 @@ def update_reddit_comments(args):
         user_agent=api_keys['reddit'][2])
     subreddit = reddit.subreddit(args.subreddit)
 
-    comment_data = {}
     # Put the reddit data into a dictionary
     for submission in subreddit.top(time_filter='all', limit=args.n):
         submission.comments.replace_more(limit=0)
-        comment_data[submission.id] = {
+        sub_comments = []
+        for comment in submission.comments.list():
+            c_entry = {
+                "num_reports": comment.num_reports,
+                "score": int(comment.score),
+                "body": norm(comment.body),
+                "ups": int(comment.ups),
+                "downs": int(comment.downs),
+                "depth": int(comment.depth)
+            }
+            sub_comments.append(c_entry)
+
+        subm_data.append({
             "title": norm(submission.title),
             "score": int(submission.score),
             "upvote_ratio": submission.upvote_ratio,
@@ -91,23 +130,13 @@ def update_reddit_comments(args):
             "author": test_redditor(submission),
             "num_comments": int(submission.num_comments),
             "selftext": norm(submission.selftext),
-            "comments": {}
-        }
-        for comment in submission.comments.list():
-            comment_data[submission.id]['comments'][comment.id] = {}
-            c_entry = comment_data[submission.id]['comments'][comment.id]
-            c_entry['num_reports'] = comment.num_reports
-            c_entry['score'] = int(comment.score)
-            c_entry['body'] = norm(comment.body)
-            c_entry['ups'] = int(comment.ups)
-            c_entry['downs'] = int(comment.downs)
-            c_entry['depth'] = int(comment.depth)
-
+            "comments": sub_comments
+        })
     # Write comment data to file
-    comment_data['num_posts'] = args.n
+    subm_data.append(args.n)
     with open(args.o, "w") as f:
-        f.write(json.dumps(comment_data))
-    return comment_data
+        f.write(json.dumps(subm_data))
+    return subm_data
 
 
 def main():
@@ -118,30 +147,20 @@ def main():
     parser = argparse.ArgumentParser(description='Analyze reddit comments')
     #parser.add_argument('-r', action='store_true', help='refresh site data')
     parser.add_argument('--subreddit', default='the_donald', help='Subreddit to examine')
-    parser.add_argument('-n', default=5, help='Number of posts to examine')
+    parser.add_argument('-n', default=10, help='Number of posts to examine')
     parser.add_argument('-o', default='../commentData.json', help='Output file name')
     parser.add_argument('-k', default='../apiKeys.json', help='Path to API keys file')
     args = parser.parse_args()
 
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '../redditNLP.json'
 
-    # Check whether to update comment data
-    comment_data = {}
-    if os.path.isfile(args.o):
-        f = open(args.o, "r")
-        comment_data = json.loads(f.read())
-        if not comment_data['num_posts'] is args.n:
-            del(comment_data)
-            comment_data = update_reddit_comments(args)
-        f.close()
-    else:
-        comment_data = update_reddit_comments(args)
+    subm_data = get_reddit_comments(args)
 
-    # Add natural laguage processing results
+    # Add natural language processing results
     nlp_calls = 0
-    for s_id in comment_data:
+    for subm in subm_data:
         # Skip validation entry
-        if s_id == "num_posts":
+        if isinstance(subm, int):
             break
         # Try each request until it works
         while True:
@@ -149,35 +168,34 @@ def main():
                 aggregate = ''
                 nlp_calls += 1
                 x = time.time()
-                comment_data[s_id]['entities'] = get_entity_sentiment(comment_data[s_id]['selftext'])
-                print "Called NLP Service. %s total calls. Call took %ss." % (nlp_calls, time.time()-x)
+                subm['entities'] = get_entity_sentiment(subm['selftext'])
+                print "Called NLP Service. %s total calls. Call took %0.3fs." % (nlp_calls, time.time()-x)
 
-                aggregate += comment_data[s_id]['selftext']
+                aggregate += subm['selftext']
 
-                for c_id in comment_data[s_id]['comments']:
-                    c = comment_data[s_id]['comments'][c_id]
+                for c in subm['comments']:
                     nlp_calls += 1
                     x = time.time()
-                    c['entities'] = get_entity_sentiment(comment_data[s_id]['selftext'])
-                    print "Called NLP Service. %s total calls. Call took %ss." % (nlp_calls, time.time()-x)
+                    c['entities'] = get_entity_sentiment(c['body'])
+                    print "Called NLP Service. %s total calls. Call took %0.3fs." % (nlp_calls, time.time()-x)
 
                     aggregate += c['body']
 
-                comment_data[s_id]['agg_entities'] = get_entity_sentiment(aggregate)
+                subm['agg_entities'] = get_entity_sentiment(aggregate)
                 nlp_calls += 1
                 print "Called NLP Service. %s total calls." % nlp_calls
-            except errors.RetryError as te:
-                print "Network is acting up again, let's give it a sec: %s" % te
+            except errors.RetryError:
+                print "The network is acting up again, let's give it a moment"
                 time.sleep(30)
                 continue
             break
 
-    print "Program made %s NLP calls" % nlp_calls
+    print "Made %s NLP calls" % nlp_calls
     del os.environ['GOOGLE_APPLICATION_CREDENTIALS']
 
     # Write complete dataset to file
     with open(args.o, "w") as f:
-        f.write(json.dumps(comment_data))
+        f.write(json.dumps(subm_data))
     exit()
 
 
