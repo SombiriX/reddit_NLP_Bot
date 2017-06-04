13a1c4d7367763b2ceabb2fb098dedaf5a738f07
Sombiri Enwemeka <sombiri@enwemeka.com>
2017-06-03 23:17:56 -0700

    - Replaced watson NLU with google cloud platform language processing     - Google's query limit is WAY higher than IBM's - Using path modification to add / remove google api keys during runtime - Moved aggregation code to its own function - Added check for existing comment data to prevent uneccesary queries to reddit - Added try/except to NLP query section with while:True loop to catch and retry queries during network connectivy issues.     - Query performance isn't great (1 sec  per 2400 queries) and my internet connection is not that good. - Cleaned up code a bit

--- databt.py
+++ databt.py
@@ -4,27 +4,18 @@ Author Sombiri Enwemeka
 '''
 import sys
 import os
-import watson_developer_cloud
-import watson_developer_cloud.natural_language_understanding.features.v1 as features
 import praw
 import json
 import argparse
+import time
 from unicodedata import normalize
+from operator import itemgetter
+from google.cloud.gapic.language.v1beta2 import enums
+from google.cloud.gapic.language.v1beta2 import language_service_client
+from google.cloud.proto.language.v1beta2 import language_service_pb2
+from google.gax import errors
+
 sys.path.append(os.path.join(os.getcwd(), '..'))
-
-
-def get_watson_nlu(username, password):
-    if (username is None or password is None or
-            username is "" or password is ""):
-        return None
-    watson_nlu = {
-        "url": "https://gateway.watsonplatform.net/natural-language-understanding/api",
-        "username": username,
-        "password": password}
-    return watson_developer_cloud.NaturalLanguageUnderstandingV1(
-        version='2017-02-27',
-        username=watson_nlu["username"],
-        password=watson_nlu["password"])
 
 
 def norm(text):
@@ -41,110 +32,153 @@ def test_redditor(submission):
     return norm(submission.author.name)
 
 
+def get_entity_sentiment(text):
+    '''
+    Detects entity sentiment in the provided text.
+    '''
+    language_client = language_service_client.LanguageServiceClient()
+    document = language_service_pb2.Document()
+
+    #if isinstance(text, six.binary_type):
+    #    text = text.decode('utf-8')
+
+    document.content = text.encode('utf-8')
+    document.type = enums.Document.Type.PLAIN_TEXT
+
+    encoding = enums.EncodingType.UTF32
+    if sys.maxunicode == 65535:
+        encoding = enums.EncodingType.UTF16
+
+    result = language_client.analyze_entity_sentiment(document, encoding)
+
+    e_list = []
+    for entity in result.entities:
+        if abs(entity.sentiment.magnitude - 0.0) > 0.001:
+            e_list.append({
+                "type": entity.type,
+                "name": entity.name,
+                "salience": entity.salience,
+                "sent_score": entity.sentiment.score,
+                "sent_mag": entity.sentiment.magnitude})
+    return sorted(e_list, key=itemgetter('salience'), reverse=True)
+
+
+def update_reddit_comments(args):
+    api_keys = None
+    try:
+        with open(args.k, 'r') as fp:
+            api_keys = json.load(fp)
+    except IOError as oops:
+        print "Error loading Reddit Authentication\r\n%s" % oops
+        exit()
+
+    # Get all comment text from top n subreddit posts
+    reddit = praw.Reddit(
+        client_id=api_keys['reddit'][0],
+        client_secret=api_keys['reddit'][1],
+        user_agent=api_keys['reddit'][2])
+    subreddit = reddit.subreddit(args.subreddit)
+
+    comment_data = {}
+    # Put the reddit data into a dictionary
+    for submission in subreddit.top(time_filter='all', limit=args.n):
+        submission.comments.replace_more(limit=0)
+        comment_data[submission.id] = {
+            "title": norm(submission.title),
+            "score": int(submission.score),
+            "upvote_ratio": submission.upvote_ratio,
+            "url": submission.url,
+            "author": test_redditor(submission),
+            "num_comments": int(submission.num_comments),
+            "selftext": norm(submission.selftext),
+            "comments": {}
+        }
+        for comment in submission.comments.list():
+            comment_data[submission.id]['comments'][comment.id] = {}
+            c_entry = comment_data[submission.id]['comments'][comment.id]
+            c_entry['num_reports'] = comment.num_reports
+            c_entry['score'] = int(comment.score)
+            c_entry['body'] = norm(comment.body)
+            c_entry['ups'] = int(comment.ups)
+            c_entry['downs'] = int(comment.downs)
+            c_entry['depth'] = int(comment.depth)
+
+    # Write comment data to file
+    comment_data['num_posts'] = args.n
+    with open(args.o, "w") as f:
+        f.write(json.dumps(comment_data))
+    return comment_data
+
+
 def main():
     '''
     Main script execution
     '''
 
-    comment_data = {}
-    f = None
-
     parser = argparse.ArgumentParser(description='Analyze reddit comments')
-    parser.add_argument('-r', action='store_true', help='refresh site data')
+    #parser.add_argument('-r', action='store_true', help='refresh site data')
     parser.add_argument('--subreddit', default='the_donald', help='Subreddit to examine')
     parser.add_argument('-n', default=5, help='Number of posts to examine')
-    parser.add_argument('-o', default='commentData.json', help='Output file name')
+    parser.add_argument('-o', default='../commentData.json', help='Output file name')
     parser.add_argument('-k', default='../apiKeys.json', help='Path to API keys file')
-
     args = parser.parse_args()
 
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '../redditNLP.json'
+
     # Check whether to update comment data
-    if args.r:
-        api_keys = None
-        try:
-            with open(args.k, 'r') as fp:
-                api_keys = json.load(fp)
-        except IOError as oops:
-            print oops
-            exit()
-
-        # Get all comment text from top n subreddit posts
-        reddit = praw.Reddit(
-            client_id=api_keys['reddit'][0],
-            client_secret=api_keys['reddit'][1],
-            user_agent=api_keys['reddit'][2])
-        subreddit = reddit.subreddit(args.subreddit)
-
-        nlu = get_watson_nlu(api_keys['watson'][0], api_keys['watson'][1])
-
-        # Put the reddit data into a dictionary
-        for submission in subreddit.top(time_filter='all', limit=args.n):
-            submission.comments.replace_more(limit=0)
-            comment_data[submission.id] = {
-                "title": norm(submission.title),
-                "score": int(submission.score),
-                "upvote_ratio": submission.upvote_ratio,
-                "url": submission.url,
-                "author": test_redditor(submission),
-                "num_comments": int(submission.num_comments),
-                "selftext": norm(submission.selftext),
-                "comments": {}
-            }
-            for comment in submission.comments.list():
-                comment_data[submission.id]['comments'][comment.id] = {}
-                c_entry = comment_data[submission.id]['comments'][comment.id]
-                c_entry['num_reports'] = comment.num_reports
-                c_entry['score'] = int(comment.score)
-                c_entry['body'] = norm(comment.body)
-                c_entry['ups'] = int(comment.ups)
-                c_entry['downs'] = int(comment.downs)
-                c_entry['depth'] = int(comment.depth)
-
-        # Write data to file, if Watson query fails file will still contain reddit data
-        with open(args.o, "w") as f:
-            f.write(json.dumps(comment_data))
-
-        # Add Watson NLU results
-        nlucalls = 0
-        for s_id in comment_data:
-            aggregate = ''
-            #comment_data[s_id]['nlu'] = nlu.analyze(
-            #    text=comment_data[s_id]['selftext'],
-            #    features=[features.Entities(), features.Keywords()],
-            #    language='en')
-            nlucalls += 1
-            aggregate += comment_data[s_id]['selftext']
-            for c_id in comment_data[s_id]['comments']:
-                c = comment_data[s_id]['comments'][c_id]
-                #print c['body']
-                #c['nlu'] = nlu.analyze(
-                #    text=c['body'],
-                #    features=[features.Entities(), features.Keywords()],
-                #    language='en')
-                nlucalls += 1
-                aggregate += c['body']
-            #comment_data[s_id]['agg_nlu'] = nlu.analyze(
-            #    text=aggregate,
-            #    features=[features.Entities(), features.Keywords()],
-            #    language='en')
-            nlucalls += 1
-
-        print "Program made %s calls to Watson" % nlucalls
-        exit()
-        # Write complete dataset to file
-        with open(args.o, "w") as f:
-            f.write(json.dumps(comment_data))
-        import ipdb
-        ipdb.set_trace()
-
+    comment_data = {}
+    if os.path.isfile(args.o):
+        f = open(args.o, "r")
+        comment_data = json.loads(f.read())
+        if not comment_data['num_posts'] is args.n:
+            del(comment_data)
+            comment_data = update_reddit_comments(args)
+        f.close()
     else:
-        try:
-            # Open existing data file
-            f = open(args.o, "rw")
-            comment_data = json.loads(f.read())
-            # Do something with data
-        except IOError as oops:
-            print oops
+        comment_data = update_reddit_comments(args)
+
+    # Add natural laguage processing results
+    nlp_calls = 0
+    for s_id in comment_data:
+        # Skip validation entry
+        if s_id == "num_posts":
+            break
+        # Try each request until it works
+        while True:
+            try:
+                aggregate = ''
+                nlp_calls += 1
+                x = time.time()
+                comment_data[s_id]['entities'] = get_entity_sentiment(comment_data[s_id]['selftext'])
+                print "Called NLP Service. %s total calls. Call took %ss." % (nlp_calls, time.time()-x)
+
+                aggregate += comment_data[s_id]['selftext']
+
+                for c_id in comment_data[s_id]['comments']:
+                    c = comment_data[s_id]['comments'][c_id]
+                    nlp_calls += 1
+                    x = time.time()
+                    c['entities'] = get_entity_sentiment(comment_data[s_id]['selftext'])
+                    print "Called NLP Service. %s total calls. Call took %ss." % (nlp_calls, time.time()-x)
+
+                    aggregate += c['body']
+
+                comment_data[s_id]['agg_entities'] = get_entity_sentiment(aggregate)
+                nlp_calls += 1
+                print "Called NLP Service. %s total calls." % nlp_calls
+            except errors.RetryError as te:
+                print "Network is acting up again, let's give it a sec: %s" % te
+                time.sleep(30)
+                continue
+            break
+
+    print "Program made %s NLP calls" % nlp_calls
+    del os.environ['GOOGLE_APPLICATION_CREDENTIALS']
+
+    # Write complete dataset to file
+    with open(args.o, "w") as f:
+        f.write(json.dumps(comment_data))
+    exit()
 
 
 if __name__ == "__main__":
